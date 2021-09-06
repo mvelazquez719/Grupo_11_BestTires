487266f49d40244aee090a9799a0f88af9cc6995
FunesLucas <funeslucas@outlook.com>
2021-09-06 18:06:26 -0300

    cambios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,11 +3,12 @@ const usersController = {
         res.render("register");
     },
     
-     login:(req, res) => {
-        res.render("login");
-     }
+    login:(req, res) => {
+       res.render("login");
+    }
     
     
 }
 
 module.exports= usersController;
+
