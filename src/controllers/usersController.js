71aad3ceef2e6d3732b1639fff3286c3e73220b3
FunesLucas <funeslucas@outlook.com>
2021-10-09 23:59:26 -0300

    Add: Express validation Part 1

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const {validationResult} = require ('express-validator');
+
 
 const usuariosPath = path.join(__dirname, '../model/users.json');
 let users = JSON.parse(fs.readFileSync(usuariosPath, 'utf-8'));
@@ -10,27 +12,14 @@ const usersController = {
     register: (req, res) => {
         res.render('register');
     },
-    storee: (req, res) => {
+    processRegister: (req, res) => {
+        const resultValidation = validationResult(req)
         
-        let objetoo = {
-			id: users.length + 1,
-			nombre: req.body.firstName,
-			apellido: req.body.lastName,
-			nombreusuario: req.body.nameUsers,
-			email: req.body.email,
-			contrasenia: req.body.password,
-            contrasenia2: req.body.passwordConfirmacion,
-			
-		};
-        console.log(objetoo)
-
-        users.push(objetoo);
-		let usuariosJSON = JSON.stringify(users);
-		fs.writeFileSync(usuariosPath, usuariosJSON),JSON.stringify(users, null, 4),
-        {encoding:"utf-8"};
-
-		
-        
+        if (resultValidation.errors.length > 0){
+            return res.render ('register',{
+                errors:resultValidation.mapped()  //<---- mapped ( vuelve el Array de errores a un objeto literal)
+            });
+        }
     },
     
     login:(req, res) => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const mainController= require("../controllers/mainController");
 const usersController= require("../controllers/usersController");
 const productsController= require("../controllers/productsController");
-const upload = require ('../middleware/multermidd')
+const upload = require ('../middleware/multermidd');
+const {body} = require ('express-validator');
+const validations = [
+    body('firstName').notEmpty().withMessage('Tienes que escribir un nombre'),
+    body('lastName').notEmpty().withMessage('Tienes que escribir un apellido'),
+    body('email').notEmpty().withMessage('Tienes que escribir un correo electronico'),
+    body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+
+    
+]
 
 router.get("/", mainController.index);
 
@@ -14,11 +23,14 @@ router.get("/carrito", productsController.carrito);
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get("/cargaProduc",productsController.cargaProduc);
-router.post("/cargaProduc",upload.single('img'),productsController.store);
+router.post("/cargaProduc",upload.single('img') ,productsController.store);
+
 
 /*** REGISTER ***/ 
 router.get("/register", usersController.register);
-router.post("/register", usersController.storee);
+/*** PROCESA REGISTER ***/ 
+router.post("/register",upload.single('img'),validations, usersController.processRegister);
+
 
 /*** GET ONE PRODUCT ***/ 
 router.get('/productID/:id', productsController.productID);
